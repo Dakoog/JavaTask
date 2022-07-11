f863080abc254abfe9b2778ac520e4ced9d81ab0
Dakoog <taktoja@spoko.pl>
2022-07-11 19:57:10 +0200

    add FifthTask code

---
+++ src/main/java/inputOutput/tasks/TaskFifth.java
@@ -0,0 +1,14 @@
+package inputOutput.tasks;
+/*
+Wyświetl w konsoli kody ósemkowe, dziesiątkowe i szesnastkowe wielkich liter alfabetu łacińskiego. W pierwszym wierszu umieść opisy poszczególnych kolumn:
+Znak,OCT, DEC i HEX.*/
+public class TaskFifth {
+    public static void main(String[] args) {
+        System.out.printf("Znak|%11s|%11s|%11s|%n","OCT","DEC","HEX");
+        System.out.println("----|-----------|-----------|-----------|");
+        char[] characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+        for (char c : characters) {
+            System.out.printf("%1$3c |%2$10o |%2$10d |%2$10X |%n",c,(int)c);
+        }
+    }
+}
