4f963af053bd70bcd4493bd0ab2932923cc675b0
Dakoog <taktoja@spoko.pl>
2022-07-11 09:41:35 +0200

    modify code.

--- src/main/java/inputOutput/tasks/ThirdTask.java
+++ src/main/java/inputOutput/tasks/ThirdTask.java
@@ -4,13 +4,13 @@ package inputOutput.tasks;
 Napisz aplikację, która wyświetli w konsoli w trzech kolumnach liczby naturalne 2, 3,
 5, 7, 11, 13 i 17, pierwiastki kwadratowe i pierwiastki sześcienne z tych liczb. Warto-
 ści pierwiastków wyświetlaj z dokładnością do 8 miejsc po przecinku, w kolumnach
-o szerokości 15 znaków System.out.printf("Number: %3d|%15.8f|%15.8f|%n", i, Math.sqrt(i), Math.cbrt(i));
+o szerokości 15 znaków
 */
 public class ThirdTask {
     public static void main(String[] args) {
         int rangeFrom = 0;
         int rangeTo = 17;
-        for (int i = rangeFrom; i < rangeTo; i++) {
+        for (int i = rangeFrom; i <= rangeTo; i++) {
             if (isPrimeNumber(i)) {
                 System.out.printf("Number: %3d|%15.8f|%15.8f|%n", i, Math.sqrt(i), Math.cbrt(i));
             }
@@ -28,8 +28,6 @@ public class ThirdTask {
         }
         return true;
     }
-
-
 }
 
 
