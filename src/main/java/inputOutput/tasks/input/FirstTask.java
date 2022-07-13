edcc2b33f7e4f91351a210d30650778e52a709fe
Dakoog <taktoja@spoko.pl>
2022-07-13 16:18:47 +0200

    added FirstTask code which convert centigrade temperature to fahrenheit temperature and vice versa

---
+++ src/main/java/inputOutput/tasks/input/FirstTask.java
@@ -0,0 +1,31 @@
+package inputOutput.tasks.input;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* Napisz program zamieniający temperaturę podaną w stopniach Celsjusza na temperaturę wyrażoną w stopniach Fahrenheita.
+ Dane wejściowe wprowadzamy z klawiatury w postaci liczby dziesiętnej; wynik należy obliczyć i wyświetlić z dokładnością do
+0,1 stopnia*/
+public class FirstTask {
+    public static void main(String[] args) throws IOException {
+
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(input);
+
+        //1) Convert temperature Centigrade to temperature Fahrenheit.
+        double temperatureCentigrade, temperatureFahrenheit;
+        System.out.print("Please insert temperature in \u00b0C, t = ");
+        temperatureCentigrade = Double.parseDouble(br.readLine()); //throwing input output exception to code client -> psvm
+        temperatureFahrenheit = 1.8 * temperatureCentigrade + 32;
+
+        System.out.printf("%.1f\u00b0C = %.1f\u00b0F %n", temperatureCentigrade,temperatureFahrenheit);
+
+       //2) Convert temperature Fahrenheit to temperature Centigrade .
+        System.out.print("Please insert temperature in \u00b0F, t = ");
+        temperatureFahrenheit = Double.parseDouble(br.readLine());
+        temperatureCentigrade = 5*(temperatureFahrenheit - 32)/9;
+
+        System.out.printf("%.1f\u00b0F = %.1f\u00b0C %n",temperatureFahrenheit, temperatureCentigrade);
+    }
+}
