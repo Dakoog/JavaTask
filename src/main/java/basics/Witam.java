f965d38718ebd805e9b516efdc06b3d7daa79342
Dakoog <taktoja@spoko.pl>
2022-07-15 10:09:16 +0200

    adding Witam class

---
+++ src/main/java/basics/Witam.java
@@ -0,0 +1,7 @@
+package basics;
+/*Napisz polskojęzyczną wersję klasycznego przykładu klasy Hello.*/
+public class Witam {
+    public static void main(String[] args) {
+        System.out.println("Witaj, świecie!\nUczę się programować w języku Java.");
+    }
+}
