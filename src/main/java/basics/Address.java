90569e8b94da6b7b32d9862a759f13363ac0ccd6
Dakoog <taktoja@spoko.pl>
2022-07-15 10:13:40 +0200

    adding Address class

---
+++ src/main/java/basics/Address.java
@@ -0,0 +1,10 @@
+package basics;
+
+public class Address {
+    public static void main(String[] args) {
+        System.out.println("""
+                Daniel Konrad Ogiński
+                ul. Seraf 5 /IV
+                32-020 Wieliczka""");
+    }
+}
