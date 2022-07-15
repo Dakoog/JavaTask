d6478098329e34bbdb68cb4d190c2f94b79bc4c2
Dakoog <taktoja@spoko.pl>
2022-07-15 10:02:46 +0200

    adding basics package with Hello class

---
+++ src/main/java/basics/Hello.java
@@ -0,0 +1,8 @@
+package basics;
+/*2.1 Przepisz dokładnie kodźródłowy podany na listingu 1. Skompiluj go i uruchom*/
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
