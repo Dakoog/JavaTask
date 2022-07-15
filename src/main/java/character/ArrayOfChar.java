ae378237c2de7e7da8f406cbd9b5302082df1225
Dakoog <taktoja@spoko.pl>
2022-07-15 14:37:37 +0200

    adding ArrayOfChar class with "character" package

---
+++ src/main/java/character/ArrayOfChar.java
@@ -0,0 +1,9 @@
+package character;
+/*3.1 Utwórz tablicę znaków zawierającą napis Dzień dobry.
+Napisz aplikację, która wyświetli napis w konsoli.*/
+public class ArrayOfChar {
+    public static void main(String[] args) {
+        char[] welcome = {'D','z','i','e','ń',' ','d','o','b','r','y','.'};
+        System.out.println(welcome);
+    }
+}
