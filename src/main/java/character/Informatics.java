bc09b9816a1ada42d18c57747233b8963aef770e
Dakoog <taktoja@spoko.pl>
2022-07-15 21:58:53 +0200

    adding Informatics class

--- src/main/java/character/Informatics.java
+++ src/main/java/character/Informatics.java
@@ -4,18 +4,30 @@ package character;
 znaki zawarte w tablicy w następujący sposób:
 a) pionowo — każdy znak w odrębnym wierszu,
 b) poziomo — znaki rozdzielone dodatkowymi odstępami (tzw. spacjowanie
-lub rozstrzelenie tekstu).*/
+lub rozstrzelenie tekstu)
+c) poziomo -> wszystkie znaki pisane dużą literą
+d) poziomo -> wszystkie znaki pisane małą literą */
 public class Informatics {
     public static void main(String[] args) {
         char[] informatics = {'I', 'n', 'f', 'o', 'r', 'm', 'a', 't', 'i', 'c', 's', '.'};
-        // a) pionowo -> każdy znak w odrębnym wierszu
-        for (char e : informatics) {
-            System.out.println("\t\t" + e);
+
+        System.out.println("\na)Vertically - each letter separately.\n");
+        for (char sign : informatics) {
+            System.out.println("\t\t" + sign);
         }
-        //b) poziomo -> znaki rozdzielone dodatkowymi odstępami (tzw.spacjami, czyli tekst rozstrzelony)
-        System.out.println();
-        for (char e : informatics) {
-            System.out.print(e + " ");
+
+        System.out.println("\nb)Horizontally - exploded letter");
+        for (char sign : informatics) {
+            System.out.print(sign + " ");
+        }
+        //
+        System.out.println("\nc)Each letter to upper case");
+        for (char sign : informatics) {
+            System.out.print(Character.toUpperCase(sign));
+        }
+        System.out.println("\nd)Each letter to lower case");
+        for (char sign : informatics) {
+            System.out.print(Character.toLowerCase(sign));
         }
     }
 }
