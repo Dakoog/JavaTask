caa76c2b88ffbcbf8f3da9bf86c632cea1e62360
Dakoog <taktoja@spoko.pl>
2022-07-16 09:33:36 +0200

    adding ReverseWord code that reverses the order of characters in the array

---
+++ src/main/java/character/ReverseWord.java
@@ -0,0 +1,20 @@
+package character;
+
+/*Utwórz tablicę zawierającą znaki słowa programming.
+Napisz program wyświetlający znaki zawarte w tablicy w kolejnoci odwrotnej — od końca do początku*/
+public class ReverseWord {
+    public static void main(String[] args) {
+        char[] programming = {'p', 'r', 'o', 'g', 'r', 'a', 'm', 'm', 'i', 'n', 'g', '.'};
+        System.out.println("I. Reverse fori");
+        for (int i = programming.length - 1; i >= 0; --i) {
+            System.out.print(programming[i]);
+        }
+        System.out.println("\n\nII. Replace the ending letters with the beginning ones");
+        for (int i = 0, j = programming.length - 1; i < j; i++, --j) {
+            char reverse = programming[i];
+            programming[i] = programming[j];
+            programming[j] = reverse;
+        }
+        System.out.println(programming);
+    }
+}
