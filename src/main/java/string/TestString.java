378bb485a98c3c725d2d75b4e88c9c07a72feb79
Dakoog <taktoja@spoko.pl>
2022-07-16 20:54:41 +0200

    modify TestString code.

--- src/main/java/string/TestString.java
+++ src/main/java/string/TestString.java
@@ -11,13 +11,18 @@ public class TestString {
         System.out.println(tasks.toLowerCase());// all text in lowercase
         System.out.println(tasks.indexOf('z'));// first index for element "z"
         System.out.println(tasks.indexOf("prog")); // first index for string "prog".
+        char dot = '.', questionMark = '?';
         System.out.println(tasks.replace('.', '?'));// replace characters with "." to "?"
-        System.out.println(tasks.replace("adania", "dania"));// replace string with "adania" to "dania"
-        System.out.println(tasks.replaceAll("ania", "anka"));// replace all strings with "anka" to "danka"
-        System.out.println(tasks.replaceFirst("ania", "anka"));// replace only first string with "ania" to "anka".
+        String str1 = "adania", str2 = "dania";
+        System.out.println(tasks.replace(str1, str2));// replace string with "adania" to "dania"
+        str1 = "ania";
+        str2 = "anka";
+        System.out.println(tasks.replaceAll(str1, str2));// replace all strings with "anka" to "danka"
+        System.out.println(tasks.replaceFirst(str1, str2));// replace only first string with "ania" to "anka".
         System.out.println(tasks.substring(10));// cut and give string that begin at index 10
         System.out.println(tasks.substring(10, 17)); // cut and give string that begin at index 10 and ending at index 17
-        System.out.println(tasks.concat("\b z podpowiedziami."));//concatenates the old string with the new string "\ b with hints." Warning! \ b - backspace (remove the last character before it) the old string with the new string "\ b with hints." Warning! \ b - backspace (remove the last character before it)
-        System.out.println(tasks+"\b"+ " z odpowiedziami.");//concatenates three strings
+        str1 = "\b z podpowiedziami.";
+        System.out.println(tasks.concat(str1));//concatenates the old string with the new string "\ b with hints." Warning! \ b - backspace (remove the last character before it) the old string with the new string "\ b with hints." Warning! \ b - backspace (remove the last character before it)
+        System.out.println(tasks + str1.substring(0,4) + str1.substring(5));//concatenates three strings
     }
 }
