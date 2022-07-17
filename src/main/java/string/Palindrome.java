e9b427c2d055c333a83821675005a5897f3b0fc5
Dakoog <taktoja@spoko.pl>
2022-07-17 20:58:26 +0200

    modify the Palindrome code, make changes to the isPalindrome method, and other modifications

--- src/main/java/string/Palindrome.java
+++ src/main/java/string/Palindrome.java
@@ -1,50 +1,53 @@
 package string;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Scanner;
 
 public class Palindrome {
     public static void main(String[] args) throws IOException {
         File palindrome = new File("palindrome.txt");
-
-        InputStreamReader input = new InputStreamReader(System.in);
-        BufferedReader br = new BufferedReader(input);
-
-        String text;
-        System.out.println("Please, type your text if you want to check if that word or sentence is a palindrome");
-
-        do {
-            System.out.println("Your text it: ");
-            text = br.readLine();
-            text = text.toLowerCase();
-
-            if (text.equalsIgnoreCase("finish")) {
-                System.out.println("The end. See you!");
-            } else {
-                if (isPalindrome(text)) {
-                    System.out.println(text.toUpperCase() + ": it is palindrome.");
-                } else {
-                    System.err.println(text.toLowerCase() + ": it isn't palindrome");
-                }
+        try (Scanner scanner = new Scanner(palindrome)) {
+            while (scanner.hasNext()) {
+                String palindromeContent = scanner.nextLine();
+                System.out.println(palindromeContent);
             }
-        } while (!text.equals("finish"));
+        }
+        try (InputStreamReader input = new InputStreamReader(System.in)) {
 
-        input.close();
+            BufferedReader br = new BufferedReader(input);
+            System.out.println();
+            String text;
+            System.out.println("Please, type your text if you want to check if that word or sentence is a palindrome");
+            boolean stop = false;
+            do {
+                System.out.println("Your text it: ");
+                text = br.readLine();
+                text = text.toLowerCase();
+
+                if (text.equalsIgnoreCase("finish")) {
+                    stop = true;
+                    System.out.println("The end. See you!");
+                } else {
+                    if (isPalindrome(text)) {
+                        System.out.println(text.toUpperCase() + ": it is palindrome.");
+                    } else {
+                        System.err.println(text.toLowerCase() + ": it isn't palindrome");
+                    }
+                }
+            } while (!stop);
+
+        }
     }
 
     public static boolean isPalindrome(String word) {
         word = word.replace(" ", "");
-        char[] arrayJavaProgramming = word.toCharArray();
-
-        for (int i = 0, j = word.length() - 1; i < j; i++, --j) {
-            char reverse = arrayJavaProgramming[i];
-            arrayJavaProgramming[i] = arrayJavaProgramming[j];
-            arrayJavaProgramming[j] = reverse;
+      int wordlength = word.length();
+        for (int i = 0; i < wordlength / 2; i++) {
+            if (word.charAt(i) != word.charAt(wordlength - 1 - i)) {
+                return false;
+            }
         }
-        String reverseWord = new String(arrayJavaProgramming);
-        return reverseWord.equals(word);
+
+        return true;
     }
 }
