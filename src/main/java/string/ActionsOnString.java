dc1401f4a6e23e2bd1740fdc9ebace376c32c0e7
Dakoog <taktoja@spoko.pl>
2022-07-17 13:07:00 +0200

    adding ActionsOnString code.

--- src/main/java/string/ActionsOnString.java
+++ src/main/java/string/ActionsOnString.java
@@ -1,9 +1,11 @@
 package string;
+
 /* 4.4 Utwórz łańcuch znaków "java programming curse.", a następnie: 	r
     a) zamień pierwszą literę na dużą
     b) zamień wszystkie litery na duże
 4.5 c) napisz ten łańcuch wspak
-4.5  */
+4.6 c) napisz ten łańcuch wspak wykorzystując zamiana liter końcowych na początkowe
+*/
 public class ActionsOnString {
     public static void main(String[] args) {
         String javaProgramming = "java programming course.";
@@ -12,14 +14,28 @@ public class ActionsOnString {
         javaProgramming = javaProgramming.toUpperCase().charAt(0) + javaProgramming.substring(1);
         System.out.println("First letter to uppercase: " + javaProgramming);
 
-         //b) all to uppercase
+        //b) all to uppercase
         System.out.println("All letters to uppercase: " + javaProgramming.toUpperCase());
 
         // c) reverse
         System.out.print("Reverse: ");
         for (int i = javaProgramming.length() - 1; i >= 0; --i) {
             System.out.print(javaProgramming.charAt(i));
-        }
 
+        }
+        // c) reverse -> alternative-> steps:
+        // 1). change string to array characters
+        char[] arrayJavaProgramming = javaProgramming.toCharArray();
+        for (int i = 0, j = javaProgramming.length() - 1; i < j; i++, --j) {
+        // 2). iteration and replace i to j
+            char reverse = arrayJavaProgramming[i];
+            arrayJavaProgramming[i] = arrayJavaProgramming[j];
+            arrayJavaProgramming[j] = reverse;
+
+        }
+        // 3). change array characters to string
+        javaProgramming = new String(arrayJavaProgramming);
+        System.out.println("\nReverse: " + javaProgramming);
     }
+
 }
