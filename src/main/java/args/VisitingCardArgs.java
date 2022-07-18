37ee73983ddd3809e677fc603a632dcde64c278c
Dakoog <taktoja@spoko.pl>
2022-07-19 00:06:41 +0200

    adding VisitingCardArgs code.

---
+++ src/main/java/args/VisitingCardArgs.java
@@ -0,0 +1,12 @@
+package args;
+
+public class VisitingCardArgs {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("\t******* "+args[0]+ " *******");
+        System.out.println("\t* "+args[1]+ " *");
+        System.out.println("\t***** "+args[2]+ " *****");
+        System.out.println("\t**************************");
+
+    }
+}
