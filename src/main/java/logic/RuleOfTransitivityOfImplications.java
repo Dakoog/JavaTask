7dd814e156b7ad629b6ea2e3fd50a1901eb8a12d
Dakoog <taktoja@spoko.pl>
2022-07-19 21:44:01 +0200

    modify RuleOfTransitivityOfImplications code - adding inner method impl().

--- src/main/java/logic/RuleOfTransitivityOfImplications.java
+++ src/main/java/logic/RuleOfTransitivityOfImplications.java
@@ -2,22 +2,21 @@ package logic;
 
 // 6.6 Napisz program, który przedstawi w tabelę dowód, że reguła przechodniości implikacji jest prawem.
 public class RuleOfTransitivityOfImplications {
+
+
     public static void main(String[] args) {
         Boolean[] bl = {true, false};
         System.out.println();
         System.out.println("\t\t\t\t\t\t\t\t\t\tRule of transitivity of implications.");
         System.out.println("|\tp\t|\tq\t|\tr\t|\t(p => q)\t|\t(q => r)\t|\t(p => q) ∧ (q => r)\t|\t(p => r)\t|\t[(p => q) ∧ (q =>r)] => (p => r)\t|");
         for (Boolean p : bl) {
+            boolean v, s;
             for (Boolean q : bl) {
                 for (Boolean r : bl) {
-                    boolean v = (impl(p, q) && impl(q, r));
-                    boolean s = impl(p, r);
-
-                    boolean u = Boolean.compare(p, r) < 1;
-                    boolean x = Boolean.compare(v, u) < 1;
-
+                    v = (impl(p, q) && impl(q, r));
+                    s = impl(p, r);
                     System.out.println(" " + p + "\t" + " " + q + "\t" + " " + r + "\t\t  " + impl(p, q) + "\t\t\t "
-                            + impl(q, r) + "\t\t\t\t"  + v + "\t\t\t\t" + " " + s + " \t\t\t\t\t  " + impl(v, s));
+                            + impl(q, r) + "\t\t\t\t" + v + "\t\t\t\t" + " " + s + " \t\t\t\t\t  " + impl(v, s));
                 }
             }
         }
