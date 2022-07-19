1a8f88e064acffedeaaa4cf99d17b2db305a567e
Dakoog <taktoja@spoko.pl>
2022-07-19 21:45:33 +0200

    adding Implications code which has impl() method.

--- src/main/java/logic/Implication.java
+++ src/main/java/logic/Implication.java
@@ -8,7 +8,7 @@ public class Implication {
         System.out.println("|\tp\t| \tq\t |\tp => q\t|");
         for (boolean p : bl) {
             for (boolean q : bl) {
-                System.out.println(" " + p + " \t " + q + "\t\t " + (Boolean.compare(p, q) < 1));
+                System.out.println(" " + p + " \t " + q + "\t\t " + Implications.impl(p,q));
             }
         }
         System.err.println(" Conclusion: The implication is only false when preceding sentence is true and sentence follow is false.");
