dd97504301e733a437a5659c282f547a294971f9
Dakoog <taktoja@spoko.pl>
2022-07-19 19:26:11 +0200

    modify code - adding commentaries

--- src/main/java/logic/Implication.java
+++ src/main/java/logic/Implication.java
@@ -1,16 +1,16 @@
 package logic;
-//∧,∨,∼,⇒,⇔ ⊕
+
+//Napisz program, który przedstawi tabelę wartości logicznych implikacji
 public class Implication {
     public static void main(String[] args) {
         boolean[] bl = {true, false};
-        System.out.println("\t\t Implication: ");
+        System.out.println("\t Implication (p => q): ");
         System.out.println("|\tp\t| \tq\t |\tp => q\t|");
         for (boolean p : bl) {
-
             for (boolean q : bl) {
-
-                System.out.println(" "+p + " \t "+q +"\t\t " + (Boolean.compare(p, q) <1));
+                System.out.println(" " + p + " \t " + q + "\t\t " + (Boolean.compare(p, q) < 1));
             }
         }
+        System.err.println(" Conclusion: The implication is only false when preceding sentence is true and sentence follow is false.");
     }
 }
