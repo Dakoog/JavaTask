48c74b52d060391fabdf763bc0a33e2dcc19dbed
Dakoog <taktoja@spoko.pl>
2022-07-20 21:59:13 +0200

    modify DeMorgansLaw code (logical signs).

--- src/main/java/logic/DeMorgansLaw.java
+++ src/main/java/logic/DeMorgansLaw.java
@@ -6,7 +6,7 @@ public class DeMorgansLaw {
         boolean[] bool = {true, false};
         System.out.println();
         System.out.println("\t\t\tThe negation of the conjunction is equivalent an alternative to denial");
-        System.out.println("|\tp \t|\tq \t|\tp ʌ q \t|\t~(p ʌ q )\t|\t~p\t|\t~q\t|\t ~p v ~q \t|\t~(p ʌ q ) <=> ~p v ~q \t|");
+        System.out.println("|\tp \t|\tq \t|\tp ʌ q \t|\t~(p ∧ q )\t|\t~p\t|\t~q\t|\t ~p v ~q \t|\t~(p ∧ q ) <=> ~p v ~q \t|");
         for (boolean p : bool)
             for (boolean q : bool) {
                 System.out.println(" " + p + " \t " + q + "\t\t" + (p && q) + " \t\t " + !(p && q) + " \t\t "
@@ -14,7 +14,7 @@ public class DeMorgansLaw {
             }
         System.out.println();
         System.out.println("\t\t\tThe negation of the alternative is equivalent a conjunction to denial");
-        System.out.println("|\tp \t|\tq \t|\tp v q \t|\t~(p v q )\t|\t~p\t|\t~q\t|\t ~p ʌ ~q \t|\t~(p v q ) <=> ~p ʌ ~q \t|");
+        System.out.println("|\tp \t|\tq \t|\tp v q \t|\t~(p v q )\t|\t~p\t|\t~q\t|\t ~p ∧ ~q \t|\t~(p v q ) <=> ~p ∧ ~q \t|");
         for (boolean p : bool)
             for (boolean q : bool) {
                 System.out.println(" " + p + " \t " + q + "\t\t" + (p || q) + " \t\t " + !(p || q) + " \t\t "
