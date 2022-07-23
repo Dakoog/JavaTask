f323972fbf741870fe93a06f1e071840402220d1
Dakoog <taktoja@spoko.pl>
2022-07-23 21:49:39 +0200

    adding SquareArea code, which calculate area and perimeter of square.

---
+++ src/main/java/conditional_statements/SquareArea.java
@@ -0,0 +1,31 @@
+package conditional_statements;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*13.2.
+Napisz program obliczający pole powierzchni i obwód kwadratu.
+Długość boku kwadratu użytkownik wprowadzi z klawiatury.
+Program powinien sprawdzić, czy wprowadzona długość boku jest poprawna (dodatnia).
+
+*/
+public class SquareArea {
+    public static void main(String[] args) throws IOException {
+        System.out.println("\n\t\t\tCALCULATE AREA AND PERIMETER OF SQUARE.");
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(input);
+        System.out.print("Please, insert length of the side of the square. Remember, it is positive number! a = ");
+        Double a = Double.parseDouble(br.readLine());
+        input.close();
+        double squareArea = a * a;
+        double perimeterOfSquare = 4 * a;
+        if (a < 0) {
+            System.out.println("Remember, length of the side of the square  it is positive number!");
+        } else {
+            System.out.printf("The area of a square with a side a = %.1f is %.2f. %nHis perimeter is %.2f ", a, squareArea, perimeterOfSquare);
+        }
+    }
+
+
+}
