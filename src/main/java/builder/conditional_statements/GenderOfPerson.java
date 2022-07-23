b6cb581cd2013ef4be8e14268bc2565c2157e994
Dakoog <taktoja@spoko.pl>
2022-07-23 11:15:20 +0200

    adding GenderOfPerson code, which determines the gender of the person with a given name

---
+++ src/main/java/builder/conditional_statements/GenderOfPerson.java
@@ -0,0 +1,32 @@
+package builder.conditional_statements;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*Napisz program, który zapyta użytkownika o imię, a nastpępbie  wykorzystując podane
+imię, wyświetli w konsoli napis w postaci: "Jan jest męczyzną." lub "Anna jest
+kobietą.*/
+public class GenderOfPerson {
+    public static void main(String[] args) throws IOException {
+
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(input);
+
+        System.out.print("Can you give me your name? Your name it: ");
+        String name = br.readLine();
+        name = name.substring(0, 1).toUpperCase().concat(name.substring(1).toLowerCase());
+        input.close();
+
+        char lastSign = name.charAt(name.length() - 1);
+        boolean barnaba = name.equalsIgnoreCase("barnaba");
+        boolean kuba = name.equalsIgnoreCase("kuba");
+        boolean bonawentura = name.equalsIgnoreCase("bonawentura");
+
+        if (lastSign == 'a' && !(barnaba || kuba || bonawentura)) {
+            System.out.printf("Hi, %s. You are woman. How are you?", name);
+        } else {
+            System.out.printf("Hi, %s. You are man. How are you?", name);
+        }
+    }
+}
