f8600f606d0699513d49f390b694351c52ddabf4
Dakoog <taktoja@spoko.pl>
2022-07-29 23:21:26 +0200

    adding OnlyPositiveNumbers code, that types only positive numbers.

---
+++ src/main/java/dowhile/OnlyPositiveNumbers.java
@@ -0,0 +1,32 @@
+package dowhile;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class OnlyPositiveNumbers {
+    public static void main(String[] args) throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(input);
+        double a;
+
+
+        do {
+            System.out.print("Please, insert positive real number, a = ");
+            a = Double.parseDouble(br.readLine());
+
+        } while (a <= 0);
+        System.out.println("Thank you, " + a + " is positive number.");
+
+        System.out.print("Please, insert natural number , b = ");
+        int b = Integer.parseInt(br.readLine());
+
+        System.out.print("\nMultiples of " + b + " less than 100 it: ");
+        do {
+            System.out.print(b + ", ");
+            b += b;
+
+        } while (b < 100);
+        System.out.println("\b\b");
+    }
+}
