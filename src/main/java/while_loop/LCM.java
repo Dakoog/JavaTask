6925461d72fa85dda0d8f6ad7e2aecf1a5a3bd53
Dakoog <taktoja@spoko.pl>
2022-08-07 15:41:36 +0200

    modifying code -> corrected try-catch instruction.

--- src/main/java/while_loop/LCM.java
+++ src/main/java/while_loop/LCM.java
@@ -9,29 +9,46 @@ public class LCM {
 
         try (InputStreamReader input = new InputStreamReader(System.in);
              BufferedReader br = new BufferedReader(input)) {
-            System.out.print("Please, insert two positive natural numbers.\na = ");
-            int a = Integer.parseInt(br.readLine());
-            System.out.print("b = ");
-            int b = Integer.parseInt(br.readLine());
-            System.out.printf("LCM (%d , %d) = ", a, b);
+            int a,b;
+            String text = "ok";
+            do {
+                try { System.out.print("Please, insert two positive natural numbers.\na = ");
 
-            if (a > b) {
-                int lcm = a;
+                     a = Integer.parseInt(br.readLine());
+                    System.out.print("b = ");
+                     b = Integer.parseInt(br.readLine());
+                     if(a<=0 || b<=0){
+                         text = "Given number is(numbers are) negative. ";
+                         System.out.println(text);
+                     }else {
+                        System.out.printf("LCM (%d , %d) = ", a, b);
+                        int lcm;
+                        if (a > b) {
+                            lcm = a;
 
-                while (lcm % b != 0) {
-                    lcm += a;
+                            while (lcm % b != 0) {
+                                lcm += a;
+                            }
+                        } else {
+                            lcm = b;
+
+                            while (lcm % a != 0) {
+                                lcm += b;
+                            }
+
+                        }
+                         System.out.println(lcm);
+                    }
+
+                } catch (NumberFormatException nfe) {
+                    text = "This isn't positive natural number.Please, try again.";
+                    System.out.println(text);
+
                 }
-                System.out.println(lcm);
-            } else {
-                int lcm = b;
 
-                while (lcm % a != 0) {
-                    lcm += b;
-                }
-                System.out.println(lcm);
+                }while (!text.equals("ok"));
+
             }
 
-
-        }
     }
 }
