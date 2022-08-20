d2c9c95e345a775548e851e3510dc486be98e6fd
Dakoog <taktoja@spoko.pl>
2022-08-20 19:04:34 +0200

    modifying code -adding new method (public Fraction(String)).

--- src/main/java/fractions/Fraction.java
+++ src/main/java/fractions/Fraction.java
@@ -1,8 +1,13 @@
 package fractions;
 
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
 public class Fraction {
     private int numerator;
     private int denominator;
+
 
     public Fraction(int a, int b) {
 
@@ -19,48 +24,72 @@ public class Fraction {
 
     }
 
+    public Fraction() {
+        this.numerator = 0;
+        this.denominator = 1;
+    }
+
     public int getNumerator() {
-        return numerator;
+        return this.numerator;
     }
 
     public int getDenominator() {
-        return denominator;
+        return this.denominator;
+    }
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+
+    public void setDenominator(int denominator) {
+        this.denominator = denominator;
     }
 
     public Fraction addingFractions(Fraction y) {
         return new Fraction(numerator * y.denominator + denominator * y.numerator, y.denominator * denominator);
     }
+
     public Fraction addingFractions(int c) {
-        return new Fraction(c* denominator + numerator,  denominator);
+        return new Fraction(c * denominator + numerator, denominator);
     }
+
     public Fraction subtractionFractions(Fraction y) {
         return new Fraction(numerator * y.denominator - denominator * y.numerator, y.denominator * denominator);
     }
+
     public Fraction subtractionFractionsFrontInteger(int c) {
 
         return new Fraction(c * denominator - numerator, denominator);
     }
+
     public Fraction subtractionFractionsBackInteger(int c) {
 
-        return new Fraction( numerator-c * denominator , denominator);
+        return new Fraction(numerator - c * denominator, denominator);
     }
+
     public Fraction multiplyFractions(Fraction y) {
         return new Fraction(this.numerator * y.numerator, this.denominator * y.denominator);
     }
+
     public Fraction multiplyFractions(int c) {
-        return new Fraction(c* numerator,  denominator);
+        return new Fraction(c * numerator, denominator);
     }
+
     public Fraction divisionFractions(Fraction y) {
 
         return new Fraction(this.numerator * y.denominator, this.denominator * y.numerator);
     }
+
     public Fraction divisionFractionsFrontInteger(int c) {
-        return new Fraction(c* denominator,  numerator);
+        return new Fraction(c * denominator, numerator);
     }
+
     public Fraction divisionFractionsBackInteger(int c) {
-        return new Fraction(numerator,c* denominator  );
+        return new Fraction(numerator, c * denominator);
     }
+
     public Fraction reducingFraction() {
+
         int divide = Utils.GCD(numerator, denominator);
         if (divide > 1) {
             this.numerator = numerator / divide;
@@ -90,12 +119,12 @@ public class Fraction {
                     return "(%d %s)".formatted(counter, y);
                 } else {
                     y = new Fraction(rest, denominator);
-                    return "(%d %s)".formatted(counter, y);
+                    return "%d %s".formatted(counter, y);
                 }
 
 
             } else
-                return "(%s)".formatted(y);
+                return "%s".formatted(y);
         }
 
 
@@ -116,5 +145,59 @@ public class Fraction {
             return numerator + "/" + denominator;
         }
 
+    }
+
+
+    public Fraction(String str) {
+        int i = str.indexOf('.');
+        if (i == -1) {
+            i = str.indexOf(',');
+        }
+
+        if (i == -1) {
+            i = str.indexOf('/');
+            if (i == -1) {
+                this.numerator = Integer.parseInt(str);
+                this.denominator = 1;
+            } else {
+                this.numerator = Integer.parseInt(str.substring(0, i));
+                this.denominator = Integer.parseInt(str.substring(i + 1));
+                if (this.denominator == 0)
+                    throw new ArithmeticException("Don't divide by 0!");
+            }
+        } else {
+            this.numerator = Integer.parseInt(str.substring(0, i) +
+                    str.substring(i + 1));
+            int k = str.length() - i;
+            this.denominator = 1;
+            int n = 1;
+            while (n++ < k)
+                this.denominator *= 10;
+        }
+
+        this.reducingFraction();
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Fraction fraction = (Fraction) o;
+        Fraction x = new Fraction(numerator, denominator);
+        Fraction y = new Fraction(fraction.numerator, fraction.denominator);
+        x.reducingFraction();
+        y.reducingFraction();
+
+        return x.numerator == y.numerator && x.denominator == y.denominator;
+    }
+
+
+    @Override
+    public int hashCode() {
+        Fraction x = new Fraction(numerator, denominator);
+        x.reducingFraction();
+        return Objects.hash(x.numerator, x.denominator);
     }
 }
